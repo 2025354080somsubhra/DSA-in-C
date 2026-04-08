4dad4d71a60e35615f97c96f84057d8cdba06d78
Somsubhra Chatterjee <2025354080.somsubhra@ug.sharda.ac.in>
2026-04-08 20:23:11 +0530

    menu driven program of Linked List

--- menuLL.c
+++ menuLL.c
@@ -7,23 +7,39 @@ struct Node {
 };
 
 typedef struct Node n;
+
 n *Head = NULL;
+n *Tail = NULL;
+
 
 void append() {
-    n *temp = (n *) malloc(sizeof(n));
-    printf("Enter value: ");
-    scanf("%d",&temp->data);
-    temp->link = NULL;
+    int k, val;
 
-    if(Head == NULL) {
-        Head = temp;
-    } else {
-        n *p = Head;
-        while(p->link != NULL)
-            p = p->link;
-        p->link = temp;
+    printf("How many elements you want to add: ");
+    scanf("%d", &k);
+
+    for(int i = 0; i < k; i++) {
+        printf("Enter value %d: ", i + 1);
+        scanf("%d", &val);
+
+        n *temp = (n *) malloc(sizeof(n));
+        if(temp == NULL) {
+            printf("Memory allocation failed\n");
+            return;
+        }
+
+        temp->data = val;
+        temp->link = NULL;
+
+        if(Head == NULL) {
+            Head = Tail = temp;   
+        } else {
+            Tail->link = temp;    
+            Tail = temp;          
+        }
     }
 }
+
 
 void display() {
     n *p = Head;
@@ -40,6 +56,7 @@ void display() {
     printf("NULL\n");
 }
 
+
 int lenNodes() {
     int count = 0;
     n *p = Head;
@@ -51,17 +68,23 @@ int lenNodes() {
     return count;
 }
 
+
 void addBegin() {
     n *temp = (n *)malloc(sizeof(n));
+
     printf("Enter value: ");
     scanf("%d",&temp->data);
 
     temp->link = Head;
     Head = temp;
+
+    if(Tail == NULL)   
+        Tail = temp;
 }
 
+
 void insert() {
-    int i=1, loc;
+    int i = 1, loc;
     n *temp = (n *)malloc(sizeof(n));
 
     printf("Enter value: ");
@@ -78,19 +101,26 @@ void insert() {
     if(loc == 1) {
         temp->link = Head;
         Head = temp;
+        if(Tail == NULL)
+            Tail = temp;
     } else {
         n *p = Head;
         while(i < loc-1) {
             p = p->link;
             i++;
         }
+
         temp->link = p->link;
         p->link = temp;
+
+        if(temp->link == NULL)  // inserted at last
+            Tail = temp;
     }
 }
 
+
 void deleteNode() {
-    int i=1, loc;
+    int i = 1, loc;
 
     if(Head == NULL) {
         printf("List is empty\n");
@@ -108,6 +138,10 @@ void deleteNode() {
     if(loc == 1) {
         n *temp = Head;
         Head = Head->link;
+
+        if(Head == NULL)   
+            Tail = NULL;
+
         free(temp);
     } else {
         n *p = Head;
@@ -115,11 +149,17 @@ void deleteNode() {
             p = p->link;
             i++;
         }
+
         n *q = p->link;
         p->link = q->link;
+
+        if(q->link == NULL) 
+            Tail = p;
+
         free(q);
     }
 }
+
 
 void mid_len() {
     int len = lenNodes();
@@ -135,14 +175,14 @@ void mid_len() {
         printf("Middle position: %d\n", (len+1)/2);
 }
 
-int main() {
-    int choice;
 
-    printf("Linked List Functions using Switch Case\n");
+int main() {
+    int ch;
+
+    printf("Linked List Menu Driven Program\n");
 
     while(1) {
-        printf("\n Linked List Operations \n");
-        printf("1. Append Node\n");
+        printf("\n1. Append \n");
         printf("2. Display List\n");
         printf("3. Count Nodes\n");
         printf("4. Add at Beginning\n");
@@ -150,19 +190,28 @@ int main() {
         printf("6. Delete Node\n");
         printf("7. Find Middle Position\n");
         printf("8. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d",&choice);
 
-        switch(choice) {
-            case 1: append(); break;
-            case 2: display(); break;
-            case 3: printf("Total Nodes = %d\n", lenNodes()); break;
-            case 4: addBegin(); break;
-            case 5: insert(); break;
-            case 6: deleteNode(); break;
-            case 7: mid_len(); break;
+        printf("Enter your choice: ");
+        scanf("%d",&ch);
+
+        switch(ch) {
+            case 1: append(); 
+                    break;
+            case 2: display(); 
+                    break;
+            case 3: printf("Total Nodes = %d\n", lenNodes()); 
+                    break;
+            case 4: addBegin(); 
+                    break;
+            case 5: insert(); 
+                    break;
+            case 6: deleteNode(); 
+                    break;
+            case 7: mid_len(); 
+                    break;
             case 8: exit(0);
-            default: printf("Invalid Choice!\n");
+            default: 
+                   printf("Invalid Choice!\n");
         }
     }
     return 0;
